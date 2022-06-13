927073f021cb7b71e7942fbd2d62f4e927c6cd60
wutno <aaron@installgentoo.net>
2022-06-13 13:42:33 -0400

    more work

--- printer.cpp
+++ printer.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
-
 #include <iostream>
 
 #include <unicode/ucnv.h>
@@ -13,33 +12,94 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 
+enum SizeSetting {
+	one = 0x31,
+	two = 0x32,
+	three = 0x33,
+	four = 0x34,
+};
+
+SizeSetting y{SizeSetting::one};
+SizeSetting x{SizeSetting::one};
+
+bool contr{false};
+
+bool findControl(UChar32 str)
+{
+	if (str == 0x1B) {
+		contr = true;
+		return true;
+	}
+
+	if (str == 0x11) {
+		y = SizeSetting::two;
+		x = SizeSetting::two;
+		return true;
+	}
+
+	if (str == 0x14) {
+		y = SizeSetting::one;
+		x = SizeSetting::one;
+		return true;
+	}
+
+	return false;
+}
+
+int resize(int size, SizeSetting scale)
+{
+	if (scale == SizeSetting::one) {
+		return size;
+	}
+
+	return size * ((static_cast<int>(scale) - 0x30) * 0.75);
+}
+
 int main()
 {
 	std::vector<uint8_t> data(255, 0);
-	std::string filename{"/home/wutno/cards/print.bin"};
-
+	std::string filename{"/home/wutno/cardsb/print.bin"};
 	std::ifstream card(filename.c_str(), std::ifstream::in | std::ifstream::binary);
-
 	card.read(reinterpret_cast<char *>(&data[0]), data.size());
 	card.close();
-
 	if (data.empty()) {
+		std::cerr << "Could not open print.bin\n";
+		return 1;
+	}
+
+	TTF_Init();
+	TTF_Font *font = TTF_OpenFont("/home/wutno/cardsb/kochi-gothic-subst.ttf", 42);
+	if (font == nullptr) {
+		std::cerr << "Could not open font\n";
+		return 1;
+	}
+
+	SDL_Color color = {0x64, 0x64, 0x96, 0xFF};
+	SDL_Surface *cardImage = IMG_Load("15.png");
+	if (cardImage == nullptr) {
+		std::cerr << "Could not open 15.png\n";
+		return 1;
+	}
+
+	UErrorCode err{U_ZERO_ERROR};
+	UConverter *ucnv = ucnv_open("ibm-942", &err);
+	if (err != U_ZERO_ERROR) {
+		std::cerr << "Could not start ICU\n";
 		return 1;
 	}
 
 	std::vector<std::string> lines(255, {0});
-
 	int i = 0;
 	while (true) {
-		auto loca = std::find(data.begin(), data.end(), 0x0D);
+		auto location = std::find(data.begin(), data.end(), 0x0D);
 
-		if (loca != std::end(data)) {
-			lines.at(i).assign(data.begin(), loca);
-			data.erase(data.begin(), loca + 1);
+		if (location != std::end(data)) {
+			lines.at(i).assign(data.begin(), location);
+			data.erase(data.begin(), location + 1);
 		} else {
 			if (!data.empty()) {
 				lines.at(i).assign(data.begin(), data.end());
-				data.erase(data.begin(), data.end()); //cleanup
+				data.erase(data.begin(), data.end()); // remove \r
 			}
 			break;
 		}
@@ -47,79 +107,59 @@ int main()
 		i++;
 	}
 
-	icu::UnicodeString fullu = ":: Fullwidth-Halfwidth;";
-	icu::UnicodeString halfu = ":: Halfwidth-Fullwidth;";
-
-	TTF_Init();
-	TTF_Font *font = TTF_OpenFont("/home/wutno/Projects/YACardEmu/build/kochi-gothic-subst.ttf", 42);
-	if (font == nullptr) {
-		return 1;
-	}
-
-	SDL_Color color = {0x64, 0x64, 0x96, 0xFF};
-	SDL_Surface *cardImage = IMG_Load("1.png");
-	if (cardImage == nullptr) {
-		return 1;
-	}
-
-	UErrorCode err{U_ZERO_ERROR};
-	UConverter *ucnv = ucnv_open("ibm-942", &err);
-
-	if (err != U_ZERO_ERROR) {
-		return 1;
-	}
-
-	int horz = 105, virt = 100, cbuffsize = 255;
-
-	int advance = 0;
-	TTF_GlyphMetrics(font, '\r', NULL, NULL, NULL, NULL, &advance);
+	int horz = 85, virt = 110, cbuffsize = 255;
+	int advance = TTF_FontLineSkip(font);
+	bool hasDoubleSize{false};
 
 	for (const auto &src : lines) {
-		icu::UnicodeString str{};
+		icu::UnicodeString str(src.c_str(), "shift_jis");
 
-		size_t reqSize = sizeof(UChar) * src.size();
-		UChar *srcBuffer = (UChar *)std::malloc(sizeof(UChar) * src.size());
-		ucnv_toUChars(ucnv, srcBuffer, reqSize, src.c_str(), src.size(), &err);
-		str.setTo(srcBuffer, reqSize);
-		free(srcBuffer);
+		size_t position{};
+		while (position < str.length()) {
+			std::printf("%d", position);
+			std::cout << ":" << str.length() << std::endl;
 
-		if (str.indexOf('\x11') != -1) {
-			TTF_SetFontSize(font, 42);
-			TTF_GlyphMetrics(font, '\r', NULL, NULL, NULL, NULL, &advance);
-			advance = advance + 18;
-			str.removeBetween(str.indexOf('\x11'), str.lastIndexOf('\x11') + 1);
-		} else {
-			//TTF_SetFontStyle(font, TTF_STYLE_NORMAL);
-			TTF_SetFontSize(font, 34);
-			TTF_GlyphMetrics(font, '\r', NULL, NULL, NULL, NULL, &advance);
-			advance = advance + 3;
+			while (findControl(str.char32At(position))) {
+				if (contr) {
+					if (str.char32At(++position) == 0x73) { // font size
+						y = static_cast<SizeSetting>(str.char32At(++position));
+						x = static_cast<SizeSetting>(str.char32At(++position));
+						str.remove((position = position - 3), 4);
+					}
+					// FIXME: font selection
+				} else {
+					hasDoubleSize = true;
+					str.remove(position, 1);
+				}
+			}
+			contr = false;
+
+			SDL_Surface *textSurf = TTF_RenderGlyph32_Blended(font, str.char32At(position), color);
+			position++;
+			if (textSurf == nullptr) {
+				continue;
+			}
+
+			// surface we use to use to resize textSurf
+			SDL_Surface *newText = SDL_CreateRGBSurface(
+				0,
+				resize(textSurf->clip_rect.w, x),
+				resize(textSurf->clip_rect.h, y),
+				32, 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000
+			);
+
+			SDL_BlitScaled(textSurf, NULL, newText, NULL);
+
+			SDL_Rect location = {horz, virt, 0, 0};
+			SDL_BlitSurface(newText, NULL, cardImage, &location);
+			SDL_FreeSurface(textSurf);
+
+			horz += (newText->clip_rect.w / 1.25) + 2;
 		}
 
-		bool hasReset{};
-		icu::UnicodeString str2{};
-		if (str.indexOf('\x14') > -1) {
-			//icu::UnicodeString str2{};
-			str.extractBetween(str.indexOf('\x14') + 1, str.length() + 1, str2);
-			str.setCharAt(str.indexOf('\x14'), '\x00'); // FIXME: bad bad
-			//str.removeBetween(str.indexOf('\x14'), str.getCapacity());
-			//str.truncate(str.indexOf('\x14') - 1);
-			hasReset = true;
-		}
-
-		SDL_Rect location = {horz, virt, 0, 0};
-		SDL_Surface *textSurf = TTF_RenderUNICODE_Blended(font, (const Uint16 *)str.getBuffer(), color);
-		SDL_BlitSurface(textSurf, NULL, cardImage, &location);
-		if (hasReset) {
-			TTF_SetFontSize(font, 34);
-			SDL_Rect location = {textSurf->clip_rect.w + (advance * 1.7), virt + (advance / 5.7), 0, 0};
-			SDL_Surface *textSurf2 = TTF_RenderUNICODE_Blended(font, (const Uint16 *)str2.getBuffer(), color);
-			SDL_BlitSurface(textSurf2, NULL, cardImage, &location);
-			SDL_FreeSurface(textSurf2);
-			TTF_SetFontSize(font, 42);
-		}
-		SDL_FreeSurface(textSurf);
-
-		virt += advance;
+		virt += advance + 1 + (hasDoubleSize ? 15 : 0);
+		horz = 85;
+		hasDoubleSize = false;
 	}
 
 	IMG_SavePNG(cardImage, "file.png");
