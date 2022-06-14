754476c9fb31acf71d40510462fa56bf342f8c85
wutno <aaron@installgentoo.net>
2022-06-13 21:22:23 -0400

    work

--- printer.cpp
+++ printer.cpp
@@ -12,6 +12,9 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 
+#define DEFAULT_X 90
+#define DEFAULT_Y 110
+
 enum SizeSetting {
 	one = 0x31,
 	two = 0x32,
@@ -19,46 +22,13 @@ enum SizeSetting {
 	four = 0x34,
 };
 
-SizeSetting y{SizeSetting::one};
-SizeSetting x{SizeSetting::one};
-
-bool contr{false};
-
-bool findControl(UChar32 str)
-{
-	if (str == 0x1B) {
-		contr = true;
-		return true;
-	}
-
-	if (str == 0x11) {
-		y = SizeSetting::two;
-		x = SizeSetting::two;
-		return true;
-	}
-
-	if (str == 0x14) {
-		y = SizeSetting::one;
-		x = SizeSetting::one;
-		return true;
-	}
-
-	return false;
-}
-
-int resize(int size, SizeSetting scale)
-{
-	if (scale == SizeSetting::one) {
-		return size;
-	}
-
-	return size * ((static_cast<int>(scale) - 0x30) * 0.75);
-}
+SizeSetting yScale{SizeSetting::one};
+SizeSetting xScale{SizeSetting::one};
 
 int main()
 {
 	std::vector<uint8_t> data(255, 0);
-	std::string filename{"/home/wutno/cardsb/print.bin"};
+	std::string filename{"/home/wutno/cardsb/print2.bin"};
 	std::ifstream card(filename.c_str(), std::ifstream::in | std::ifstream::binary);
 	card.read(reinterpret_cast<char *>(&data[0]), data.size());
 	card.close();
@@ -68,7 +38,7 @@ int main()
 	}
 
 	TTF_Init();
-	TTF_Font *font = TTF_OpenFont("/home/wutno/cardsb/kochi-gothic-subst.ttf", 42);
+	TTF_Font *font = TTF_OpenFont("/home/wutno/cardsb/kochi-gothic-subst.ttf", 35);
 	if (font == nullptr) {
 		std::cerr << "Could not open font\n";
 		return 1;
@@ -88,94 +58,106 @@ int main()
 		return 1;
 	}
 
-	std::vector<std::string> lines(255, {0});
-	int i = 0;
-	while (true) {
-		auto location = std::find(data.begin(), data.end(), 0x0D);
 
-		if (location != std::end(data)) {
-			lines.at(i).assign(data.begin(), location);
-			data.erase(data.begin(), location + 1);
-		} else {
-			if (!data.empty()) {
-				lines.at(i).assign(data.begin(), data.end());
-				data.erase(data.begin(), data.end()); // remove \r
-			}
-			break;
-		}
+	std::string test(data.begin(), data.end());
+	icu::UnicodeString str(test.c_str(), "shift_jis");
 
-		i++;
-	}
+	size_t position{};
+	UChar32 lastChar{};
+	UChar32 currentChar{};
 
-	int horz = 85, virt = 110, cbuffsize = 255;
-	int advance = TTF_FontLineSkip(font);
-	bool hasDoubleSize{false};
-	bool fourteen{false};
+	// Modified in loop
+	int xLocation = DEFAULT_X;
+	int yLocation = DEFAULT_Y;
 
-	for (const auto &src : lines) {
-		icu::UnicodeString str(src.c_str(), "shift_jis");
+	int tallestGlyph{};
 
-		size_t position{};
-		while (position < str.length()) {
-			std::printf("%d", position);
-			std::cout << ":" << str.length() << std::endl;
+	bool lastUsedScale{false};
+	bool usedScale{false};
 
-			UChar32 lastChar{};
-			UChar32 currentChar{};
-			while (findControl(str.char32At(position))) {
-				currentChar = str.char32At(position);
-				if (contr) {
-					if (str.char32At(++position) == 0x73) { // font size
-						y = static_cast<SizeSetting>(str.char32At(++position));
-						x = static_cast<SizeSetting>(str.char32At(++position));
-						str.remove((position = position - 3), 4);
+	int lineSkip = TTF_FontHeight(font);
+
+	while (position < str.length()) {
+		currentChar = str.char32At(position);
+		SDL_Surface *glyph = TTF_RenderGlyph32_Blended(font, currentChar, color);
+
+		switch (currentChar) {
+			case 0x0D:
+				{
+					if (usedScale) {
+						lastUsedScale = true;
+						yLocation += lineSkip * 2;
+					} else {
+						lastUsedScale = false;
+						yLocation += lineSkip * 1.15;
 					}
-					// FIXME: font selection
-				} else {
-					if (str.char32At(position) == 0x11) {
-						hasDoubleSize = true;
-						if (lastChar == 0x14) {
-							horz = 85;
-							virt += advance + 1 + (hasDoubleSize ? 17 : 0);
+
+					xLocation = DEFAULT_X;
+					usedScale = false;
+					position++;
+				}
+				continue;
+			case 0x11:
+				// FIXME: Hack for MT3
+				if (lastChar == 0x14) {
+					yLocation += lineSkip * 1.65;
+					xLocation = DEFAULT_X;
+				}
+				// FIXME: Single line titles cause issues so we need to skip an extra line but this could happen in other places
+				if (lastUsedScale) {
+					yLocation += lineSkip * 1.05;
+				}
+				yScale = SizeSetting::two;
+				xScale = SizeSetting::two;
+				position++;
+				lastChar = currentChar;
+				usedScale = true;
+				lastUsedScale = true;
+				continue;
+			case 0x14:
+				yScale = SizeSetting::one;
+				xScale = SizeSetting::one;
+				position++;
+				lastChar = currentChar;
+				continue;
+			case 0x1B:
+				{
+					if (str.char32At(++position) == 0x73) { // font size
+						yScale = static_cast<SizeSetting>(str.char32At(++position));
+						xScale = static_cast<SizeSetting>(str.char32At(++position));
+						str.remove((position = position - 2), 4);
+
+						if (yScale != SizeSetting::one) {
+							usedScale = true;
 						}
 					}
-					str.remove(position, 1);
 				}
-				lastChar = currentChar;
-			}
-			contr = false;
-
-			bool isSpace{false};
-			if (str.char32At(position)) {
-				isSpace = true;
-			}
-
-			SDL_Surface *textSurf = TTF_RenderGlyph32_Blended(font, str.char32At(position), color);
-			position++;
-			if (textSurf == nullptr) {
 				continue;
-			}
-
-			// surface we use to use to resize textSurf
-			SDL_Surface *newText = SDL_CreateRGBSurface(
-				0,
-				resize(textSurf->clip_rect.w, x),
-				resize(textSurf->clip_rect.h, y),
-				32, 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000
-			);
-
-			SDL_BlitScaled(textSurf, NULL, newText, NULL);
-
-			SDL_Rect location = {horz, virt, 0, 0};
-			SDL_BlitSurface(newText, NULL, cardImage, &location);
-			SDL_FreeSurface(textSurf);
-
-			horz += (newText->clip_rect.w / 1.25) + 2;
+			default:
+				break;
 		}
 
-		virt += advance + 1 + (hasDoubleSize ? 17 : 0);
-		horz = 85;
-		hasDoubleSize = false;
+		SDL_Surface *newGlyph = SDL_CreateRGBSurface(
+			0,
+			glyph->clip_rect.w * (static_cast<int>(xScale) - 0x30),
+			glyph->clip_rect.h * (static_cast<int>(yScale) - 0x30),
+			32, 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000
+		);
+
+		SDL_BlitScaled(glyph, NULL, newGlyph, NULL);
+
+		SDL_Rect location = {xLocation, yLocation, 0, 0};
+		SDL_BlitSurface(newGlyph, NULL, cardImage, &location);
+
+		xLocation += newGlyph->clip_rect.w;
+
+		tallestGlyph = (tallestGlyph < newGlyph->clip_rect.h ? newGlyph->clip_rect.h : tallestGlyph);
+
+		SDL_FreeSurface(glyph);
+		SDL_FreeSurface(newGlyph);
+
+		lastChar = currentChar;
+		position++;
 	}
 
 	IMG_SavePNG(cardImage, "file.png");
