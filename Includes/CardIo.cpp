GXTX/YACardEmu

commit b21620a63513f3c32de8fb6b71f87beb4bb4df5c

Save config when card is changed

Includes/CardIo.cpp
@@ -609,6 +609,7 @@ void CardIo::ClearCardData()
 	cardData.resize(NUM_TRACKS);
 }
 
+// FIXME: Move these into a filesystem access class which will allow better flexability if someone wanted to use NFC cards
 void CardIo::ReadCard()
 {
 	std::string fullPath = m_cardSettings->cardPath + m_cardSettings->cardName;
@@ -678,7 +679,13 @@ void CardIo::WriteCard()
 		}
 	}
 
-	// TODO: Handle if cardPath doesn't have a trailing slash
+	if (m_cardSettings->cardPath.back() != '/' || m_cardSettings->cardPath.back() != '\\') {
+#ifdef _WIN32
+		m_cardSettings->cardPath.append("/");
+#else
+		m_cardSettings->cardPath.append("\\");
+#endif
+	}
 	auto fullPath = m_cardSettings->cardPath + m_cardSettings->cardName;
 
 	std::string writeBack;
@@ -698,6 +705,13 @@ void CardIo::WriteCard()
 		card.open(fullPath, std::ofstream::out | std::ofstream::binary);
 		card.write(writeBack.c_str(), writeBack.size());
 		card.close();
+
+		// FIXME: This is dirty
+		mINI::INIFile config("config.ini");
+		mINI::INIStructure ini;
+		config.read(ini);
+		ini["config"]["autoselectedcard"] = m_cardSettings->cardName;
+		config.write(ini);
 	}
 
 	ClearCardData();

Includes/CardIo.h
@@ -35,6 +35,8 @@
 #include <iomanip>
 #include <regex>
 
+#include "mini/ini.h"
+
 #include "spdlog/spdlog.h"
 #include "spdlog/async.h"
 #include "spdlog/fmt/bin_to_hex.h"

config.ini.sample
@@ -1,6 +1,5 @@
 [config]
-; basepath MUST end in a trailing slash!
-basepath = /home/wutno/cards/
+;basepath = /home/wutno/cards/
 ;basepath = C:\Temp\cards\
 ; Linux
 ;serialpath = /dev/ttyUSB0
@@ -9,7 +8,7 @@ basepath = /home/wutno/cards/
 ;serialpath = \\.\pipe\YACardEmu
 ; Which device should we emulate?
 ; C1231LR / C1231BR / S31R
-targetdevice = C1231LR
+;targetdevice = C1231LR
 ;
 ; Optional
 ;

main.cpp
@@ -78,6 +78,7 @@ void SigHandler(int sig)
 	}
 }
 
+// TODO: Handle this elsewhere, feels dirty
 bool ReadConfig()
 {
 	// Read in config values

commit 58d08d3b488c88d179c659671e8297359613e736

Fix broken filename generation caused by b21620a

Includes/CardIo.cpp
@@ -679,13 +679,14 @@ void CardIo::WriteCard()
 		}
 	}
 
-	if (m_cardSettings->cardPath.back() != '/' || m_cardSettings->cardPath.back() != '\\') {
 #ifdef _WIN32
+	if (m_cardSettings->cardPath.back() != '/')
 		m_cardSettings->cardPath.append("/");
 #else
+	if (m_cardSettings->cardPath.back() != '\\')
 		m_cardSettings->cardPath.append("\\");
 #endif
-	}
+
 	auto fullPath = m_cardSettings->cardPath + m_cardSettings->cardName;
 
 	std::string writeBack;